e3416718258b013550fcbd6439bca0a72df0ba19
Elisa Lou <classic.melody@gmail.com>
2013-03-12 04:10:16 +0000

    Dat moment when you relearn MVC and everyt hing comes together... WOOT

--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,12 @@
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Container;
 
+import javax.swing.JFrame;
 
-import view.SliderView;
 import model.MainViewModel;
+import view.SliderView;
+import view.ToolbarView;
 
 // should make a view just for the toolbar.. would still add as North of the BorderLayout
 //Slider should be a view too, that extends IView.
@@ -15,58 +16,18 @@ public class Window {
 	public static void main(String[] args) {
 
 		MainViewModel model = new MainViewModel();
-		final view.MainView view = new view.MainView(model);
+		final view.CanvasView view = new view.CanvasView(model);
 
 		JFrame frame = new JFrame("Assignment 3 - MVC");
 		Container content = frame.getContentPane();
 		content.setLayout(new BorderLayout());
 		frame.getContentPane().add(view, BorderLayout.CENTER);
 
-		JButton clearButton = new JButton("Clear");
-		clearButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				view.clear();
-				view.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-			}
-		});
-		
-		JButton drawToggle = new JButton("Draw");
-		drawToggle.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//stuff
-				view.setCursor(new Cursor(Cursor.HAND_CURSOR));
-			}
-		});
-		JButton eraseToggle = new JButton("Erase");
-		eraseToggle.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//change the pointer to something else
-				view.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-			}
-		});
-		JButton selectToggle = new JButton("Select");
-		selectToggle.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//change the pointer to something else
-				view.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
-			}
-		});
-		
-		JPanel panel = new JPanel();
-		panel.add(clearButton);
-		panel.add(drawToggle);
-		panel.add(eraseToggle);
-		panel.add(selectToggle);
-		content.add(panel, BorderLayout.NORTH);
-		
+		ToolbarView toolbar = new view.ToolbarView(model);
+		content.add(toolbar, BorderLayout.NORTH);
 
-		JSlider slider3 = new JSlider();
-		slider3.setBorder(BorderFactory.createTitledBorder("Yippee"));
-		slider3.setMajorTickSpacing(20);
-		slider3.setMinorTickSpacing(5);
-		slider3.setPaintTicks(true);
-		slider3.setPaintLabels(true);
-		content.add(slider3, BorderLayout.SOUTH);
+		SliderView slider = new view.SliderView(model);
+		content.add(slider, BorderLayout.SOUTH);
 	    
 		frame.pack();
 		frame.setSize(800, 600);
--- src/model/MainViewModel.java
+++ src/model/MainViewModel.java
@@ -4,61 +4,27 @@ import java.awt.geom.GeneralPath;
 // Note!  Nothing from the view package is imported here.
 import java.util.ArrayList;
 
+
 public class MainViewModel extends Object {
 	/* A list of the model's views. */
 	private ArrayList<IView> views = new ArrayList<IView>();
-
 	private ArrayList<GeneralPath> paths = new ArrayList<GeneralPath>();
+	private int state = 0; // {0,1,2} = {draw, erase, selection}. Default is draw
+	private boolean cleared = false;
 	
-	// Limit the size of the triangle.
-	public static final double MAX_SIDE = 100.0;
-	public static final double MAX_HYPO = Math.sqrt(MAX_SIDE * MAX_SIDE
-			+ MAX_SIDE * MAX_SIDE);
-
-	private double base = 50.0; // length of the base
-	private double height = 50.0; // height of the triangle
 
 	// Override the default construtor, making it private.
 	public MainViewModel() {
 	}
 
-
-	/** Set the base to a new value. Must be between 0 and MAX_BASE. */
-	public void setBase(double theBase) {
-		double tmp = Math.max(0, theBase);
-		this.base = Math.min(tmp, MAX_SIDE);
-		this.updateAllViews();
+	public void setState(int state){
+		this.state = state;
+		this.updateAllViews(); // not sure if this is necessary
 	}
-
-	/** Set the height to a new value. Must be between 0 and MAX_HEIGHT. */
-	public void setHeight(double theHeight) {
-		this.height = Math.min(Math.max(0, theHeight), MAX_SIDE);
-		this.updateAllViews();
+	
+	public int getState(){
+		return this.state;
 	}
-
-	/** Set both the base and the height to new values. */
-	public void setValues(double theBase, double theHeight) {
-		this.base = Math.min(Math.max(0, theBase), MAX_SIDE);
-		this.height = Math.min(Math.max(0, theHeight), MAX_SIDE);
-
-		this.updateAllViews();
-	}
-
-	/** Get the length of this triangle's base. */
-	public double getBase() {
-		return this.base;
-	}
-
-	/** Get this triangle's height. */
-	public double getHeight() {
-		return this.height;
-	}
-
-	/** Get the length of this triangle's hypotenuse. */
-	public double getHypotenuse() {
-		return Math.sqrt(this.base * this.base + this.height * this.height);
-	}
-
 	/** Add a new view of this triangle. */
 	public void addView(IView view) {
 		this.views.add(view);
@@ -79,5 +45,14 @@ public class MainViewModel extends Object {
 	
 	public void addPath(GeneralPath path) {
 		this.paths.add(path);
+	}
+	
+	public boolean getClear(){
+		return this.cleared;
+	}
+	
+	public void setClear(boolean toclear){
+		this.cleared = toclear;
+		this.updateAllViews();
 	}
 }
